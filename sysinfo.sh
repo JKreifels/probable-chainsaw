5b601d356066fc8feefd37786a2fbb4284a764be
Kreifels <jkreifels859@studentubuntu14.ecc.iwcc.edu>
2017-11-09 16:15:34 -0600

    First bash scripts and varibles.

---
+++ sysinfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "
+$(clear)
+Server Name: $(hostname)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Your system has been running for$(uptime | cut -d, -f1).
+
+There are$(uptime | cut -d, -f3) logged in.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+You're using $(df -h | grep dm-0 | tr -s " " | cut -d" " -f5) of your hard drive space.
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"
