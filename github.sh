3d9e1c35ae82837026a3cabeeded832851bd1959
Kreifels <jkreifels859@studentubuntu14.ecc.iwcc.edu>
2017-11-20 14:20:51 -0600

    Fixed?

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter your comment:  " comment
 cd ~/Projects/probable-chainsaw
-git add .
+git add -A .
 git commit -m "$comment"
 git push -u origin master
--- vcars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Do you prefer Ford or Chevy:  " truck
-
-case $truck in
-ford) echo "Chevy is better";;
-chevy) echo "Ford is better";;
-*) echo "Whatever"; bash vcars.sh;;
-esac
