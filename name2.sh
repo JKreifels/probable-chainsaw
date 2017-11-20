141b715efde0d55b14cd00275acb36709d4db8cc
Kreifels <jkreifels859@studentubuntu14.ecc.iwcc.edu>
2017-11-20 14:33:11 -0600

    Working?

---
+++ name2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo ""
+read -p "Who are you? " name   
+read -p "How old are you? " age
+read -p "What is your favorite color? " favColor
+echo "
+Hello $name! You're $age years old and you're $1.
+You said your favorite color is $favColor."
