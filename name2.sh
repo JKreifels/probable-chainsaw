d861cdc0f23833be545fe230107e8bd23783bf88
Kreifels <jkreifels859@studentubuntu14.ecc.iwcc.edu>
2017-11-20 14:33:39 -0600

    Ok

--- name2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ""
-read -p "Who are you? " name   
-read -p "How old are you? " age
-read -p "What is your favorite color? " favColor
-echo "
-Hello $name! You're $age years old and you're $1.
-You said your favorite color is $favColor."
